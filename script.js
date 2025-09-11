31b4f1a266ba811bbcbe3ed4dbe5f857fcdfde75
lateef <117226522+latakin@users.noreply.github.com>
2025-09-11 15:29:54 +0100

    added the playgame function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let indexOfChoice = Math.floor(Math.random() * choices.length);
@@ -14,20 +15,37 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    if ( (humanChoice === "rock"  && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "scissors" ) || (humanChoice === "paper" && computerChoice === "paper")) {
-       alert(`its a tie, you both chose ${humanChoice}`) 
-    } else if ((humanChoice === "rock"  && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper" ) || (humanChoice === "paper" && computerChoice === "rock") ) {
-        alert (`You won, computer chose ${computerChoice}`)
+function playRound() {
+    const  humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    if  (humanSelection === computerSelection) {
+       alert(`its a tie, you both chose ${humanSelection}`) 
+    } else if ((humanSelection === "rock"  &&  computerSelection === "scissors")  || (humanSelection === "scissors"  &&  computerSelection === "paper") || (humanSelection === "paper"  &&  computerSelection === "rock")) {
+        alert (`You won, computer chose ${computerSelection}`)
         humanScore += 1;
-    } else {
-        alert(`Computer won, computer chose ${computerChoice}`)
+    } else if ( (humanSelection === "paper"  && computerSelection === "scissors") || (humanSelection === "scissors" && computerSelection === "rock" ) || (humanSelection === "rock" && computerSelection === "paper") ) {
+        alert(`Computer won, computer chose ${computerSelection}`)
         computerScore += 1;
     }
     
 }
-const  humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 
-playRound(humanSelection, computerSelection);
+function playGame() {
+    let gameTime = 0;
+    while ( gameTime < 5) {
+        playRound();
+        gameTime += 1;
+    }
+    if(humanScore > computerScore) {
+        alert(`You win with a score of ${humanScore} against computer's ${computerScore}  `)
+    } else {
+        alert(`Computer wins with a score of ${computerScore} against your score of ${humanScore}`)
+    }
+    
+    
+}
+
+
+playGame();
+
